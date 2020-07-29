9d7ec839594ebe8faac71efcfa82ab88a859d2ab
Jorge Cobo <kobyjc@hotmail.com>
2020-07-29 12:53:35 +0200

    Homewor-query

--- week-2/mandatory/2-ecommerce-db/cyf_ecommerce.sql
+++ week-2/mandatory/2-ecommerce-db/cyf_ecommerce.sql
@@ -103,3 +103,31 @@ INSERT INTO order_items (order_id, product_id, quantity) VALUES(8, 5, 1);
 INSERT INTO order_items (order_id, product_id, quantity) VALUES(9, 13, 2);
 INSERT INTO order_items (order_id, product_id, quantity) VALUES(10, 14, 1);
 INSERT INTO order_items (order_id, product_id, quantity) VALUES(10, 6, 5);
+
+
+select name , address from customers where country = 'United States';
+select * from customers order by name;
+select * from products where unit_price > 100;
+select * from products where product_name like '%socks%';
+select * from products order by unit_price desc limit 5;
+select product_name, unit_price, supplier_name from products join suppliers on supplier_id=suppliers.id;
+select product_name, supplier_name from products join suppliers on supplier_id=suppliers.id where suppliers.country = 'United Kingdom';
+select * from orders where customer_id=1;
+select * from orders join customers on customer_id=customers.id where customers.name='Hope Crosby';
+select product_name, unit_price, quantity from products join order_items on products.id=order_items.product_id 
+join orders on order_items.order_id=orders.id where order_reference='ORD006';
+
+--Retrieve all the products with their supplier for all orders of all customers. The result should only contain the columns name (from customer), order_reference order_date, product_name, supplier_name and quantity.--
+select name, order_reference, order_date, product_name, supplier_name, quantity from customers 
+join orders on customers.id=orders.customer_id 
+join order_items on orders.id=order_items.order_id 
+join products on order_items.product_id=products.id 
+join suppliers on products.supplier_id=suppliers.id;
+
+select name from customers 
+join orders on customers.id=orders.customer_id 
+join order_items on orders.id=order_items.order_id 
+join products on order_items.product_id=products.id 
+join suppliers on products.supplier_id=suppliers.id
+where suppliers.country = 'China';
+
